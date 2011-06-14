f0485bae020139a268662bb1b690528b255d8062
Raimond Spekking <raymond@users.mediawiki.org>
2011-06-14 20:52:22 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-06-14 20:30:00 UTC)

--- CodeEditor.i18n.php
+++ CodeEditor.i18n.php
@@ -15,3 +15,10 @@ $messages['en'] = array(
 	'codeeditor-desc' => 'Syntax-highlighted editing for JavaScript and CSS pages using [http://ace.ajax.org/ Ace editor]'
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'codeeditor-desc' => 'Ermöglicht auf Wikiseiten das Bearbeiten von JavaScript und CSS mit Syntaxhervorhebung mithilfe des [http://ace.ajax.org/ Ace-Editors]',
+);
+
