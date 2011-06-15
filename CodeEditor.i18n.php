045dcf90a0f344e8fbdd393ddf022a57d936f820
Raimond Spekking <raymond@users.mediawiki.org>
2011-06-15 20:04:37 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-06-15 19:51:00 UTC)

--- CodeEditor.i18n.php
+++ CodeEditor.i18n.php
@@ -15,10 +15,52 @@ $messages['en'] = array(
 	'codeeditor-desc' => 'Syntax-highlighted editing for JavaScript and CSS pages using [http://ace.ajax.org/ Ace editor]'
 );
 
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'codeeditor-desc' => 'Sintaks-uitgeligde redigering vir JavaScript- en CSS-bladsye met [http://ace.ajax.org/ Ace-redigeerder]',
+);
+
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author EugeneZelenko
+ */
+$messages['be-tarask'] = array(
+	'codeeditor-desc' => 'Рэдагаваньне JavaScript- і CSS-коду на старонках з падсьветкай сынтаксісу ў [http://ace.ajax.org/ Ace editor]',
+);
+
 /** German (Deutsch)
  * @author Kghbln
  */
 $messages['de'] = array(
 	'codeeditor-desc' => 'Ermöglicht auf Wikiseiten das Bearbeiten von JavaScript und CSS mit Syntaxhervorhebung mithilfe des [http://ace.ajax.org/ Ace-Editors]',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'codeeditor-desc' => 'Edición de páxinas en JavaScript e CSS con realce sintáctico que emprega o [http://ace.ajax.org/ editor Ace]',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'codeeditor-desc' => 'Уредување со истакната синтакса за JavaScript и каскадни стилски страници (CSS) користејќи го уредникот [http://ace.ajax.org/ Ace]',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'codeeditor-desc' => 'Penyuntingan tersorot sintaks untuk laman-laman JavaScript dan CSS dengan menggunakan [http://ace.ajax.org/ editor Ace]',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'codeeditor-desc' => 'Edição de páginas JavaScript ou CSS com realce sintáctico, usando o [http://ace.ajax.org/ editor Ace]',
 );
 
