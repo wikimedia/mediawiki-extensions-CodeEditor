85fdb01d00d1b71d3b280a5fca73605fa2062f26
Derk-Jan Hartman <hartman.wiki@gmail.com>
2014-01-10 11:43:25 +0100

    Enable ACE webworkers
    Now that the RL path is set correctly we can enable the webworkers
    This adds syntax checker and linting
    
    Change-Id: Ie9177d4acae45f2aadb1dfd3e66049195056bca0

--- modules/jquery.codeEditor.js
+++ modules/jquery.codeEditor.js
@@ -184,9 +184,6 @@
 					// updated right away to actually use the new style.
 					$( mw ).bind( 'LivePreviewPrepare', context.evt.codeEditorSubmit );
 
-					// Disable code-linting in the background using JavaScript WebWorkers.
-					// Currently broken due to require() / ResourceLoader mismatch.
-					session.setUseWorker( false );
 					session.setMode( new (require( "ace/mode/" + lang ).Mode) );
 
 					// Force the box to resize horizontally to match in future :D
