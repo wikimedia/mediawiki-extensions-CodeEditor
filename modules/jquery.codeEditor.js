7a396da3a1740fa68a60daa0fdabe94f658ad150
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-06-06 03:14:14 +0000

    build: Updating npm dependencies
    * eslint-config-wikimedia: 0.27.0 → 0.28.0
      The following rules are failing and were disabled:
      * modules:
        * no-jquery/no-extend
    
    * grunt-stylelint: 0.19.0 → 0.20.0
    * stylelint-config-wikimedia: 0.16.1 → 0.17.1
    
    Change-Id: Ifbde191e4d9155dbecc9c395dd89ec06c91c356a

--- modules/jquery.codeEditor.js
+++ modules/jquery.codeEditor.js
@@ -415,12 +415,12 @@
 					} );
 
 					// Use jQuery UI resizable() so that users can make the box taller
-					// eslint-disable-next-line es-x/no-resizable-and-growable-arraybuffers
+
 					container.resizable( {
 						handles: 's',
 						minHeight: $box.height(),
 						resize: function () {
-							// eslint-disable-next-line es-x/no-resizable-and-growable-arraybuffers
+
 							context.codeEditor.resize();
 						}
 					} );
