d880a58fc9e0131dc732a16c3235fcb642067dfc
Derk-Jan Hartman <hartman.wiki@gmail.com>
2025-01-07 23:51:11 +0100

    Use foreign resources for updating Ace
    Bug: T355823
    Change-Id: I012b49d451d83f990b05c4050d0cbba3560ccfaf

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,7 @@
 module.exports = function ( grunt ) {
 	const conf = grunt.file.readJSON( 'extension.json' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
-	grunt.loadNpmTasks( 'grunt-contrib-clean' );
-	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
-	grunt.loadNpmTasks( 'grunt-exec' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
@@ -39,42 +36,9 @@ module.exports = function ( grunt ) {
 				requireLowerCase: false
 			},
 			all: conf.MessagesDirs.CodeEditor
-		},
-		exec: {
-			'npm-update-ace': {
-				cmd: 'npm update ace-builds',
-				callback: function ( error, stdout, stderr ) {
-					grunt.log.write( stdout );
-					if ( stderr ) {
-						grunt.log.write( 'Error: ' + stderr );
-					}
-
-					if ( error !== null ) {
-						grunt.log.error( 'update error: ' + error );
-					}
-				}
-			}
-		},
-		clean: {
-			ace: [ 'modules/lib/ace/*' ]
-		},
-		copy: {
-			ace: {
-				expand: true,
-				cwd: 'node_modules/ace-builds/src-noconflict/',
-				src: [ '**' ],
-				dest: 'modules/lib/ace/'
-			},
-			'ace-misc-files': {
-				expand: true,
-				cwd: 'node_modules/ace-builds/',
-				src: [ 'LICENSE', 'CHANGELOG.md', 'README.md' ],
-				dest: 'modules/lib/ace/'
-			}
 		}
 	} );
 
-	grunt.registerTask( 'update-ace', [ 'exec:npm-update-ace', 'clean:ace', 'copy:ace', 'copy:ace-misc-files' ] );
 	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
